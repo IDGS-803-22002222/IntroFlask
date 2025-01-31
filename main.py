23881cf3277e5732b6cd2c4ab6f33f35a3c8d326
IDGS-803-22002222 <78643@alumnos.utleon.edu.mx>
2025-01-30 22:28:34 -0600

    Formulario calculadora web agregado

--- main.py
+++ main.py
@@ -1,11 +1,46 @@
-from flask import Flask
+from flask import Flask, render_template ,request
 
 app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hola mundo!!!"
+    grupo="IDGS803"
+    lista=["Juan","Eduardo","mario"]
+    return render_template("index.html",grupo=grupo, lista=lista)
 
+
+@app.route('/ejemplo1')
+def ejemplo1():
+    return render_template("ejemplo1.html")
+
+@app.route('/ejemplo2')
+def ejemplo2():
+    return render_template("ejemplo2.html")
+
+
+@app.route('/OperasBas')
+def OperasBas():
+    return render_template("OperasBas.html")
+
+
+@app.route('/OperasBas', methods = ["GET", "POST"])
+def funcionResultado():
+    if request.method == "POST":
+        n1=int(request.form.get("n1"))
+        n2=int(request.form.get("n2"))
+        operacion = request.form.get("operacion")
+        if operacion == "suma":
+            res="La suma de {} mas {} es: {}".format(n1,n2,(n1+n2))
+        if operacion == "resta":
+            res="La resta de {} menos {} es: {}".format(n1,n2,(n1-n2))
+        if operacion == "multi":
+            res="La multiplicacion de {} por {} es: {}".format(n1,n2,(n1*n2))
+        if operacion == "divicion":
+            res="La divicion de {} entre {} es: {}".format(n1,n2,(n1/n2))
+            
+    return render_template("OperasBas.html",res=res)
+       
+    
 
 @app.route("/hola")
 def hola():
@@ -34,6 +69,24 @@ def suma(n1,n2):
 @app.route("/default/<string:nom>")
 def fun(nom="pedro"):
     return "el nombre de nom es "+ nom
+
+
+@app.route("/form1")
+def form1():
+    return '''
+    <form>
+    <label>Nombre: </label>
+    <br>
+    <input type= "text" name="nombre" placeholder="Nombre ">
+    <br><br>
+    <label>Nombre: </label>
+    <input type= "text" name="nombre" placeholder="Nombre ">
+    <br>
+    </form>
+
+'''
+
+
 
 
 if __name__ == '__main__':
